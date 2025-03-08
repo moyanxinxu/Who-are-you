844cc094d62f01706d626b66695e0b081d4ba26f
moyanxinxu <wwxy.mail@gmail.com>
2025-03-08 16:23:14 +0000

    fix: 修复bug

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 @dataclass
 class hp:
     name = "xxxx"
-    author = "xxxxx"
-    data_path = "/root/Documents/Who-are-you/src/data/identity.json"
-    model_name = "Qwen/Qwen2.5-0.5B"
+    author = "xxxx"
+    data_path = "/path/to/identity.json"
+    model_name = "/path/to/model/dir"
     bos_token = "<|im_start|>"
 
     r = 8
@@ -14,10 +14,11 @@ class hp:
     lora_dropout = 0.1
 
     output_dir = "./output/Qwen"
+    lora_dir = "./output/lora"
     per_device_train_batch_size = 8
     gradient_accumulation_steps = 2
     logging_steps = 10
-    num_train_epochs = 3
+    num_train_epochs = 10
     gradient_checkpointing = True
     save_steps = 100
     learning_rate = 1e-4
--- src/utils/run.py
+++ src/utils/run.py
@@ -5,7 +5,6 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     DataCollatorForSeq2Seq,
-    GenerationConfig,
     Trainer,
     TrainingArguments,
 )
@@ -17,26 +16,48 @@ ds = Dataset.from_pandas(df)
 
 
 tokenizer = AutoTokenizer.from_pretrained(
-    hp.model_name,
-    use_fast=False,
-    bos_token=hp.bos_token,
-    trust_remote_code=True,
+    hp.model_name, bos_token=hp.bos_token, trust_remote_code=True
 )
 tokenizer.pad_token_id = tokenizer.eos_token_id
 
-model = AutoModelForCausalLM.from_pretrained(
-    hp.model_name,
-    trust_remote_code=True,
-)
+model = AutoModelForCausalLM.from_pretrained(hp.model_name, trust_remote_code=True)
 
 config = LoraConfig(
     task_type=TaskType.CAUSAL_LM,
     target_modules=["c_attn", "c_proj", "w1", "w2"],
-    inference_mode=False,  # 训练模式
-    r=hp.r,  # Lora 秩
-    lora_alpha=hp.lora_alpha,  # Lora alaph，具体作用参见 Lora 原理
-    lora_dropout=hp.lora_dropout,  # Dropout 比例
+    inference_mode=False,
+    r=hp.r,
+    lora_alpha=hp.lora_alpha,
+    lora_dropout=hp.lora_dropout,
 )
+
+def datapipe(example):
+    instruction = example["instruction"]
+    output = example["output"].format(name=hp.name, author=hp.author)
+
+    instruction = [
+        {"role": "user", "content": instruction},
+    ]
+
+    instruction = tokenizer.apply_chat_template(
+        instruction, tokenize=True, add_generation_prompt=True
+    )
+
+    input_ids = instruction
+    attention_mask = [1] * len(input_ids)
+    label = [-100] * len(input_ids)
+
+    output = f"{output}{tokenizer.eos_token}"
+    output = tokenizer(output, add_special_tokens=False)
+
+    input_ids += output["input_ids"]
+    attention_mask += output["attention_mask"]
+    label += output["input_ids"]
+
+    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": label}
+
+
+ds = ds.map(datapipe, remove_columns=ds.column_names)
 
 args = TrainingArguments(
     output_dir=hp.output_dir,
@@ -50,36 +71,6 @@ args = TrainingArguments(
     save_on_each_node=hp.save_on_each_node,
 )
 
-
-def datapipe(example):
-    instruction = example["instruction"]
-    output = example["output"].format(name=hp.name, author=hp.author)
-
-    instruction = [
-        {"role": "system", "content": ""},
-        {"role": "user", "content": instruction},
-    ]
-
-    instruction = tokenizer.apply_chat_template(
-        instruction, tokenize=True, add_generation_prompt=True
-    )
-
-    input_ids = instruction
-    attention_mask = [1] * len(input_ids)
-    label = [-100] * len(input_ids)
-
-    output = f"{tokenizer.bos_token}assistant\n{output}{tokenizer.eos_token}"
-    output = tokenizer(output, add_special_tokens=False)
-
-    input_ids += output["input_ids"]
-    attention_mask += output["attention_mask"]
-    label += output["input_ids"]
-
-    return {"input_ids": input_ids, "attention_mask": attention_mask, "labels": label}
-
-
-ds = ds.map(datapipe, remove_columns=ds.column_names)
-
 trainer = Trainer(
     model=model,
     args=args,
@@ -87,3 +78,6 @@ trainer = Trainer(
     data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, padding=True),
 )
 trainer.train()
+
+# model = model.merge_and_unload()
+# model.save_pretrained(hp.lora_dir)
